9e9876e21013457a6e6a4b57426caad2f07bb508
FAWRY\Youssif.Saad <youssif.saad@fawry.com>
2022-07-23 00:31:54 +0200

    Initial commit

---
+++ src/main/java/com/bsf/assessment/BsfAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.bsf.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsfAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsfAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsf/assessment/BsfAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsf.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsfAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
